d0ec3cb0dec117c8d322ce4ef6fbe27bd17a104c
jsanguinetticds <jsanguinetti@codigodelsur.com>
2019-07-22 17:13:16 -0300

    fixed orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,17 @@
 const env = require("dotenv");
-env.config();
 
 const entitiesExtension = "ts";
 const entitiesDir = "src";
 const migrationsDir = "src/migration/*.ts";
+let ssl = process.env.DATABASE_SSL && JSON.parse(process.env.DATABASE_SSL)
+if (ssl == null) ssl = (process.env.NODE_ENV == 'production')
 
 module.exports = {
   type: "postgres",
   entities: [`${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`],
   url: process.env.DATABASE_URL,
+  ssl,
   migrations: [migrationsDir],
-  ssl: true,
   cli: {
     migrationsDir: "src/migration"
   }
